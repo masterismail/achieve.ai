56368f8f5b5024146bd986cf23a34ca1de20a938
Mohammed Ismail <mohammedismail523261@gmail.com>
2023-12-16 20:11:45 +0530

    neww

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Goals from './components/GoalsSection';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <Goals />
+    </main>
+  );
+}
