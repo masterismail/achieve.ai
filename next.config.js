f9a7895b3909f909eba25f06271359467cee6694
Victor N <victorkjngo@gmail.com>
2024-01-10 11:41:50 -0800

    Update next config as per `npm run build` error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Other Next.js configuration options
+  // Other Next.js configuration options
   
+
+  // Enabling breaking site down into static HTML files per route
+    output: 'export', // https://nextjs.org/docs/advanced-features/static-html-export
     // Use distDir for specifying the export directory
     distDir: 'out', // Replace 'out' with your desired export directory name
   };
