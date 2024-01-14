a60e1d21198b234502b2db7cb29da07270f5e8d6
Victor N <victorkjngo@gmail.com>
2024-01-14 00:21:40 -0800

    Adds created_timestamp to goals Formats page based on prettier settings

--- src/app/components/goalmodal.tsx
+++ src/app/components/goalmodal.tsx
@@ -1,53 +1,51 @@
-import * as React from 'react';
-import { useState } from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import GoalType from './goalsType';
-import type { NextPage } from 'next';
-import styles from './frame.module.css';
-import supabase from '../supabaseclient';
+import * as React from "react";
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import GoalType from "./goalsType";
+import type { NextPage } from "next";
+import styles from "./frame.module.css";
+import supabase from "../supabaseclient";
 const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
 
 type FrameType = {
-  appendGoalsList: (goal: GoalType) => void
-}
+  appendGoalsList: (goal: GoalType) => void;
+};
 
-const Frame: React.FC<FrameType> = ({ 
-  appendGoalsList
-}) => {
-  const [inputValue, setInputValue] = useState('');
-  const [input1Value, setInput1Value] = useState('');
-  const [input2Value, setInput2Value] = useState('');
-  const [input3Value, setInput3Value] = useState('');
+const Frame: React.FC<FrameType> = ({ appendGoalsList }) => {
+  const [inputValue, setInputValue] = useState("");
+  const [input1Value, setInput1Value] = useState("");
+  const [input2Value, setInput2Value] = useState("");
+  const [input3Value, setInput3Value] = useState("");
 
   const [userId, setUserId] = useState<string | null>(null);
 
   const handleInsert = async () => {
     const payload = {
-    //       // Define the structure of your data here based on your table columns
-          goal_title: inputValue,
-          deadline: input1Value,
-          description: input2Value,
-      priority: input3Value,
-          created_timestamp: ''
-    }
-    console.log('submitted goal form! payload to send to supabase', payload)
-    alert("form submitted!")
-    appendGoalsList(payload)
+      //       // Define the structure of your data here based on your table columns
+      goal_title: inputValue || "example title",
+      deadline: input1Value || "example deadline",
+      description: input2Value || "example description",
+      priority: input3Value || "example priority",
+      created_timestamp: new Date().toISOString(),
+    };
+    console.log("submitted goal form! payload to send to supabase", payload);
+    alert("form submitted!");
+    appendGoalsList(payload);
     // try {
-      
+
     //   const { data, error } = await supabase.from('goals').insert([
     //     {
     //       // Define the structure of your data here based on your table columns
@@ -69,10 +67,10 @@ const Frame: React.FC<FrameType> = ({
 
   return (
     <div className={styles.frameParent}>
-      <div className={styles.groupParent} >
+      <div className={styles.groupParent}>
         <div className={styles.frameGroup}>
           <div className={styles.frameWrapper}>
-            <div className={styles.goalParent} >
+            <div className={styles.goalParent}>
               <div className={styles.goal}>Goal</div>
               <div className={styles.text14px}>goal likhna dekho</div>
               <div className={styles.textParent}>
@@ -156,41 +154,39 @@ const Frame: React.FC<FrameType> = ({
             </button>
           </div>
         </div>
-    </div>
+      </div>
     </div>
   );
 };
 
 type BasicModalType = {
-  openModal: () => void,
-  closeModal: () => void,
-  modalState: boolean,
-  appendGoalsList: (goal: GoalType) => void
-}
+  openModal: () => void;
+  closeModal: () => void;
+  modalState: boolean;
+  appendGoalsList: (goal: GoalType) => void;
+};
 
-const BasicModal: NextPage<BasicModalType> = ({ 
-  openModal, closeModal, modalState, 
+const BasicModal: NextPage<BasicModalType> = ({
+  openModal,
+  closeModal,
+  modalState,
   appendGoalsList,
 }) => {
-    return (
-      <div className={styles.container} >
-        <button onClick={openModal}  className={styles.buttondefaultprimaryiconTe}>
-          <img className={styles.iconplus} alt="" src="/Vector.svg" />
-          <div className={styles.add}>Add</div>
-        </button>
-        <Modal
-          open={modalState}
-          onClose={
-            closeModal
-          }
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >   
-          <Frame
-            appendGoalsList={appendGoalsList}
-          />
-        </Modal>
-      </div>
+  return (
+    <div className={styles.container}>
+      <button onClick={openModal} className={styles.buttondefaultprimaryiconTe}>
+        <img className={styles.iconplus} alt="" src="/Vector.svg" />
+        <div className={styles.add}>Add</div>
+      </button>
+      <Modal
+        open={modalState}
+        onClose={closeModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Frame appendGoalsList={appendGoalsList} />
+      </Modal>
+    </div>
   );
 };
 
